9b1bda8b952e56a241f99b4d7df8462ab13d0142
gatorbotics <gatorbotics1700@gmail.com>
2024-01-19 17:27:45 -0800

    shooter 3.0

--- src/main/java/frc/robot/Buttons.java
+++ src/main/java/frc/robot/Buttons.java
@@ -1,16 +1,31 @@
 package frc.robot;
 
 import frc.robot.subsystems.DrivetrainSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.ShooterSubsystem.ShooterStates;
 
 public class Buttons {
     
   private DrivetrainSubsystem m_drivetrainSubsystem = Robot.m_drivetrainSubsystem;
-  
+  private ShooterSubsystem m_shooterSubsystem = Robot.m_shooterSubsystem; //put this in robot.java
   public void buttonsPeriodic(){
-
       //driver
-      if (OI.m_controller.getBButton()){ //emergency stop EVERYTHING
+      if (OI.m_controller.getLeftBumper()){ //emergency stop EVERYTHING
         m_drivetrainSubsystem.stopDrive(); 
       }
+      if (OI.m_controller.getAButton()){ 
+        m_shooterSubsystem.setState(ShooterStates.AMP);
+      }
+      if (OI.m_controller.getYButton()){ 
+        m_shooterSubsystem.setState(ShooterStates.SPEAKER_BLUE);
+      }
+      if (OI.m_controller.getBButton()){ 
+        m_shooterSubsystem.setState(ShooterStates.SPEAKER_RED);
+      }
+      if (OI.m_controller.getRightBumper()){ 
+        m_shooterSubsystem.setState(ShooterStates.OFF);
+      }
+      
+
   }
 }
