b703c1fe22c97b4f7b1e0c6b5326f99b0a198c73
aydagokturk <25agokturk@castilleja.org>
2024-02-09 16:38:18 -0800

    deletey

--- src/main/java/frc/robot/subsystems/LimeLightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimeLightSubsystem.java
@@ -4,12 +4,9 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.NetworkTable;
 
 public class LimeLightSubsystem {
-    private final double MOUNTING_ANGLE = 2.0; //degrees change
+    private final double MOUNTING_ANGLE = 0.0; //degrees change TODO: ask build about mounting angle of shooter
     private final double SPEAKER_HEIGHT = 2.045; //This is the middle of the speaker hole
-    private final double ROBOT_HEIGHT = 20.0; //inches change
-    private final double IDEAL_DISTANCE = 40.0; //inches change
-    private static double tv;
-    private static double ty;
+    private final double ROBOT_HEIGHT = 0.0; //inches change TODO: ask build about robo height
 
     private NetworkTable networkTable = NetworkTableInstance.getDefault().getTable("limelight");
     
@@ -18,31 +15,12 @@ public class LimeLightSubsystem {
         System.out.println("pipeline:" + networkTable.getEntry("pipeline").getDouble(0.0));
     }
 
-    public void init(){
-        tv = networkTable.getEntry("tv").getDouble(0.0);//valid targets? 0 or 1
-        ty = networkTable.getEntry("ty").getDouble(0.0); //vertical offset from -20.5-20.5 degrees
-    }
-
-    public void periodic(){
-
-    }
-
-    // turning tx and ty into distance (in)
-    public double yDistanceFromIdeal(){
-        double distanceFromTarget = (SPEAKER_HEIGHT- ROBOT_HEIGHT)/Math.tan(Math.toRadians(MOUNTING_ANGLE + ty));
-        System.out.println("distance from target: " + distanceFromTarget);
-        double distanceFromIdeal = distanceFromTarget - IDEAL_DISTANCE;
-        System.out.println("distance from ideal: " + distanceFromIdeal);
-        return distanceFromIdeal;
-    }
-
-
     public double getTv(){
         return networkTable.getEntry("tv").getDouble(0.0);
     }
 
     public double getTy(){
-        return ty; //get vertical angle
+        return networkTable.getEntry("ty").getDouble(0.0); //get vertical angle
     }
 
 }
