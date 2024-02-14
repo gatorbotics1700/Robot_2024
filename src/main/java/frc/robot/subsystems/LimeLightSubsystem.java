dee789815a9f01ee0d049b70df2104be36aa36c4
dragonette13 <65913060+dragonette13@users.noreply.github.com>
2024-02-13 17:03:10 -0800

    did math to find shooter angle with new limelight position

--- src/main/java/frc/robot/subsystems/LimeLightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimeLightSubsystem.java
@@ -1,9 +1,16 @@
 package frc.robot.subsystems;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.NetworkTable;
+import frc.robot.Constants;
+
 
 public class LimelightSubsystem {
-    
+
+    public static final double HEIGHT_TO_TAG = 57.1225; //height from limelight to center of aprilTag, TODO: subtract limelight height
+    public static final double HEIGHT_TO_SPEAKER = 79.3725; //height from shooter hinge to speaker, TODO: subtract shooter height
+    public static final double LIMELIGHT_SHOOTER_DIST = 31; //lateral distance between limelight and shooter hinge, TODO: placeholder value, ask build for actual number
+    public static final double SPEAKER_DEPTH = 10; //distance from back of speaker to where we want to shoot, TODO: placeholder value, find actual number
+
     private NetworkTable networkTable = NetworkTableInstance.getDefault().getTable("limelight");
     
     public void setPipeline(double pipeline){
@@ -18,4 +25,13 @@ public class LimelightSubsystem {
     public double getTy(){
         return networkTable.getEntry("ty").getDouble(0.0); //get vertical angle (crosshair to target)
     }
+
+    public double getDesiredShooterAngle(){
+        return Math.atan(HEIGHT_TO_SPEAKER/(distToTag() + LIMELIGHT_SHOOTER_DIST - SPEAKER_DEPTH));
+    }
+
+    private double distToTag(){
+        return HEIGHT_TO_TAG/Math.tan(Math.toRadians(getTy()));
+    }
+    
 }
